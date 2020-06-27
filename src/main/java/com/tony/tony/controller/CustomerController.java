64a6b87c17a005896f7b25251748940a6621b465
tonyspark <toyongyeon@gmail.com>
2020-06-27 16:53:12 +0900

    [2020/06/27] index.html 만들기 - controller - security config 정의

---
+++ src/main/java/com/tony/tony/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package com.tony.tony.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CustomerController {
+    // todo: 로그인 화면
+    // todo: 회원관리 화면
+    // todo:
+}
---
+++ src/main/java/com/tony/tony/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.tony.tony.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
